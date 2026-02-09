75a9bd1d1dc8afc956ecf5be3dfa78208a45210c
VisneeB <visinee@gmail.com>
2026-02-09 20:59:12 +0700

    feat: enhance posts API with filtering, retrieval, updating, and deletion functionality

--- app.mjs
+++ app.mjs
@@ -26,17 +26,62 @@ app.get("/health/db", async (req, res) => {
 
 app.get("/posts", async (req, res) => {
   try {
-    const results = await connectionPool.query("select * from posts");
+    const genres = req.query.genres;
+    const keywords = req.query.keywords;
+    const PAGE_SIZE = 5;
 
-    return res.status(200).json({
-      data: results.rows,
-    });
+    let query = "select * from movies";
+    let values = [];
+
+    if (keywords && genres) {
+      query += " where genres ilike $1 and title ilike $2 limit $3";
+      values = [`%${genres}%`, `%${keywords}%`, PAGE_SIZE];
+    } else if (keywords) {
+      query += " where title ilike $1 limit $2";
+      values = [`%${keywords}%`, PAGE_SIZE];
+    } else if (genres) {
+      query += " where genres ilike $1 limit $2";
+      values = [`%${genres}%`, PAGE_SIZE];
+    } else {
+      query += " limit $1";
+      values = [PAGE_SIZE];
+    }
+    const results = await connectionPool.query(query, values);
   } catch (err) {
     console.error("DB error:", err);
     return res.status(500).json({
       message: "server cannot read post",
     });
   }
+
+  return res.status(200).json({
+    data: results.rows,
+  });
+});
+
+app.get("/posts/:postId", async (req, res) => {
+  const { postId } = req.params;
+  let results;
+
+  try {
+    results = await connectionPool.query("select * from posts where id = $1", [
+      postId,
+    ]);
+
+    if (results.rows.length === 0) {
+      return res.status(404).json({
+        message: "Server could not find a requested post",
+      });
+    }
+  } catch (err) {
+    console.error("DB error:", err);
+    return res.status(500).json({
+      message: "Server could not read post because database connection",
+    });
+  }
+  return res.status(200).json({
+    data: results.rows,
+  });
 });
 
 app.post("/posts", async (req, res) => {
@@ -72,6 +117,63 @@ app.post("/posts", async (req, res) => {
   }
 
   return res.status(201).json({ message: "Created post successfully" });
+});
+
+app.put("/posts/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const { title, image, category_id, description, content, status_id } =
+    req.body;
+  let result;
+
+  try {
+    const query = `update posts set title = $1,image =$2, category_id =$3, description =$4, content =$5, status_id =$6 where id = $7 returning*`;
+    const values = [
+      title,
+      image,
+      category_id,
+      description,
+      content,
+      status_id,
+      postId,
+    ];
+
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Server could not find a requested post to update" });
+    }
+    result = await connectionPool.query(query, values);
+  } catch (error) {
+    console.error("Create post error:", error);
+    return res.status(500).json({
+      message: `Server could not update post because database connection`,
+    });
+  }
+
+  return res.status(200).json({ message: "Updated post sucessfully" });
+});
+
+app.delete("/posts/:postId", async (req, res) => {
+  const { postId } = req.params;
+  let result;
+
+  try {
+    result = await connectionPool.query(
+      "delete from posts where id = $1 returning*",
+      [postId],
+    );
+
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Server could not find a requested post to update" });
+    }
+  } catch (error) {
+    console.error("Delete post error:", error);
+    return res.status(500).json({
+      message: `Server could not delete post because database connection`,
+    });
+  }
 });
 
 app.listen(PORT, () => {
